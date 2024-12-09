137bb5f6e73cdc3e49c21195000a126d1ad58d27
Nancy W <159850766+nancying8@users.noreply.github.com>
2024-12-09 15:29:44 -0800

    update image to url

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -5,6 +5,8 @@ import AOS from "aos"; // Import AOS library for scroll animations
 import "aos/dist/aos.css"; // Import the AOS styles
 import CardInfo from "../data/CardInfo.json" // Importing project data from the JSON file
 
+
+
 function HomeCard() {
   // Initialize AOS when the component is mounted
   useEffect(() => {
