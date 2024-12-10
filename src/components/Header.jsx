cde7c4358574dd5b3b798102b01fd6cf8f5112fc
Nancy W <159850766+nancying8@users.noreply.github.com>
2024-12-09 16:08:08 -0800

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"; // Import React and useState hook
 import { Link } from "react-router-dom"; // Import Link for navigation
-import Logo from "/src/assets/logo.svg"; // Import logo image
+import Logo from "/public/assets/logo.svg"; // Import logo image
 
 function Header() {
   // State to track the menu open/close
